560e7a33599b8117c9f2c4699a885f99da8f456b
mariafernandagomezdiaz <140983416+mariafernandagomezdiaz@users.noreply.github.com>
2024-05-21 09:54:35 -0600

    week2

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+
+import StudentInfo from "./student-info";
+
+export default function Week2() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <StudentInfo />
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h2>Maria Fernanda Gomez</h2>
+            <Link href="https://github.com/mariafernandagomezdiaz/cprg306-assignments">GitHub Repository</Link>
+        </div>
+    );
+}
