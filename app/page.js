e55129106baee38eb984c9f6922c15bf1dfc4c1f
mariafernandagomezdiaz <140983416+mariafernandagomezdiaz@users.noreply.github.com>
2024-06-10 11:42:31 -0600

    week4
    added week4

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Week2() {
             <h1 className="text-xl">CPRG 306: Web Development 2 - Assignments</h1>
             <li><Link className={linkStyles} href="/week-2"> Week 2</Link></li>
             <li><Link className={linkStyles} href="/week-3"> Week 3</Link></li>
+            <li><Link className={linkStyles} href="/week-4"> Week 4</Link></li>
         </main>
     );
 }
---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import NewTitem from "./new-item";
+
+
+export default function ItemForm(){
+    return(
+        <main>
+            <NewTitem />
+        </main>
+    );
+}
