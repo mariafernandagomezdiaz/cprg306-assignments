a877a520e2c611d10440da8c1f9561134111fa9d
mariafernandagomezdiaz <140983416+mariafernandagomezdiaz@users.noreply.github.com>
2024-07-08 16:25:00 -0600

    added week 7

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ export default function Week2() {
             <li><Link className={linkStyles} href="/week-4"> Week 4</Link></li>
             <li><Link className={linkStyles} href="/week-5"> Week 5</Link></li>
             <li><Link className={linkStyles} href="/week-6"> Week 6</Link></li>
+            <li><Link className={linkStyles} href="/week-7"> Week 7</Link></li>
         </main>
     );
 }
